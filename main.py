ed5ce64fd48ac68ef6da5e8ff6bfd418681a92b8
basilbenny1002 <basilbenny1002@gmail.com>
2025-10-29 21:19:37 +0530

    implement tree serialization and deserialization methods in Tree class; update main script for tree generation and loading

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 
-from tools import collect_files
+from tools import collect_files, generate_tree, search_tree, trees
+from models import FileData, Tree
+import json
+
+loaded_trees = {}
+ROOT_DIR = r"c:/"
+
+
+generate_tree(ROOT_DIR)
+
+
+for value, tree in enumerate(trees):
+    with open("{value}tree.json", "w") as f:
+        json.dump(tree.to_dict(), f, indent=2)
+
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    with open("{value}tree.json") as f:
+        loaded_root = Tree.from_dict(json.load(f))
+        loaded_trees[letter] = loaded_root
+
+trees = loaded_trees
+search_tree("hehehe")
--- models.py
+++ models.py
@@ -7,6 +7,23 @@ class Tree:
         for letter in string.ascii_lowercase:
             setattr(self, letter, None)
 
+    def to_dict(self):
+        data = {"value": self.value, "files": self.files}
+        for letter in string.ascii_lowercase:
+            child = getattr(self, letter)
+            # if child is not None:
+            data[letter] = child.to_dict()
+        return data
+
+    @classmethod
+    def from_dict(cls, data):
+        node = cls(data["value"])
+        node.files = data["files"]
+        for letter in string.ascii_lowercase:
+            if letter in data:
+                setattr(node, letter, cls.from_dict(data[letter]))
+        return node
+
 class FileData:
     def __init__(self, name: str, path: str, type: str):
         self.file_name = name
