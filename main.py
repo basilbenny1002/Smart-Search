aca467aadcee5c8a4b27bf3ce58d7dcb27fe56b6
basilbenny1002 <basilbenny1002@gmail.com>
2025-10-30 11:37:20 +0530

    Update the json saving path update gitignore to add the folder containing the json values

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ ROOT_DIR = r"c:/"
 generate_tree(ROOT_DIR)
 
 
-for value, tree in trees.items():
-    with open(f"{value}tree.json", "w") as f:
+for letter, tree in trees.items():
+    with open(rf"trees/{letter}tree.json", "w") as f:
         json.dump(tree.to_dict(), f, indent=2)
 
-for letter in "abcdefghijklmnopqrstuvwxyz":
-    with open(f"{letter}tree.json") as f:
-        loaded_root = Tree.from_dict(json.load(f))
-        loaded_trees[letter] = loaded_root
+# for letter in "abcdefghijklmnopqrstuvwxyz":
+#     with open(f"./trees/{letter}tree.json") as f:
+#         loaded_root = Tree.from_dict(json.load(f))
+#         loaded_trees[letter] = loaded_root
 
-trees.clear()
-trees.update(loaded_trees)
-results = search_tree("hehehe")
+# trees.clear()
+# trees.update(loaded_trees)
+# results = search_tree("hehehe")
